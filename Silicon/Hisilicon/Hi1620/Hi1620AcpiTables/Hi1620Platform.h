a5295e57adeaff0c02d6509ceb9cc13b0b1b3707
Sun Yuanchen <sunyuanchen@huawei.com>
2018-06-25 20:59:18 +0800

    Silicon/Hisilicon/Acpi: Move some macro to PlatformArch.h
    ARM_ACPI_HEADER is used by a unify module in other Pkg,
    so move some macro to PlatformArch.h for unify D0x.
    
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Sun Yuanchen <sunyuanchen@huawei.com>
    Signed-off-by: Ming Huang <ming.huang@linaro.org>
    Signed-off-by: Heyi Guo <heyi.guo@linaro.org>

--- Silicon/Hisilicon/Hi1620/Hi1620AcpiTables/Hi1620Platform.h
+++ Silicon/Hisilicon/Hi1620/Hi1620AcpiTables/Hi1620Platform.h
@@ -2,7 +2,7 @@
 *
 *  Copyright (c) 2011-2015, ARM Limited. All rights reserved.
 *  Copyright (c) 2018, Hisilicon Limited. All rights reserved.
-*  Copyright (c) 2015, Linaro Limited. All rights reserved.
+*  Copyright (c) 2015-2018, Linaro Limited. All rights reserved.
 *
 *  This program and the accompanying materials
 *  are licensed and made available under the terms and conditions of the BSD License
@@ -20,28 +20,7 @@
 #ifndef _HI1620_PLATFORM_H_
 #define _HI1620_PLATFORM_H_
 
-//
-// ACPI table information used to initialize tables.
-//
-#define EFI_ACPI_ARM_OEM_ID           'H','I','S','I',' ',' '   // OEMID 6 bytes long
-#define EFI_ACPI_ARM_OEM_TABLE_ID     SIGNATURE_64 ('H','I','P','0','8',' ',' ',' ') // OEM table id 8 bytes long
-#define EFI_ACPI_ARM_OEM_REVISION     0x00000000
-#define EFI_ACPI_ARM_CREATOR_ID       SIGNATURE_32 ('I','N','T','L')
-#define EFI_ACPI_ARM_CREATOR_REVISION 0x20151124
-
-// A macro to initialise the common header part of EFI ACPI tables as defined by
-// EFI_ACPI_DESCRIPTION_HEADER structure.
-#define ARM_ACPI_HEADER(Signature, Type, Revision) {              \
-    Signature,                      /* UINT32  Signature */       \
-    sizeof (Type),                  /* UINT32  Length */          \
-    Revision,                       /* UINT8   Revision */        \
-    0,                              /* UINT8   Checksum */        \
-    { EFI_ACPI_ARM_OEM_ID },        /* UINT8   OemId[6] */        \
-    EFI_ACPI_ARM_OEM_TABLE_ID,      /* UINT64  OemTableId */      \
-    EFI_ACPI_ARM_OEM_REVISION,      /* UINT32  OemRevision */     \
-    EFI_ACPI_ARM_CREATOR_ID,        /* UINT32  CreatorId */       \
-    EFI_ACPI_ARM_CREATOR_REVISION   /* UINT32  CreatorRevision */ \
-  }
+#include <../Include/PlatformArch.h>
 
 #define HI1620_WATCHDOG_COUNT  2
 
