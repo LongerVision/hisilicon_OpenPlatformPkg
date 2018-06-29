fbae4f343e0203b17e7b2e5dd7eaa8e3aba3612e
Sun Yuanchen <sunyuanchen@huawei.com>
2018-06-29 15:16:39 +0800

    Silicon/Hisilicon/D0x: Move macro definition to PlatformArch.h
    Unify MemorySubClassDxe by Moving macro definition
    to PlatformArch.h
    
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Sun Yuanchen <sunyuanchen@huawei.com>
    Signed-off-by: Ming Huang <ming.huang@linaro.org>
    Signed-off-by: Heyi Guo <heyi.guo@linaro.org>

--- Silicon/Hisilicon/Hi1620/Include/PlatformArch.h
+++ Silicon/Hisilicon/Hi1620/Include/PlatformArch.h
@@ -26,6 +26,7 @@
 #define MAX_DIMM        2
 #define MAX_RANK_CH     8
 #define MAX_RANK_DIMM   4
+#define MAX_DIMM_SIZE   256  // In GB
 // Max NUMA node number for each node type
 #define MAX_NUM_PER_TYPE 8
 
