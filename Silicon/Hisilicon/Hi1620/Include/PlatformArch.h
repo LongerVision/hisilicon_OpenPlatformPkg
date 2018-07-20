ad8d62dd3c1a7f5fbff90d58c34d4c237398822c
Ming Huang <ming.huang@linaro.org>
2018-07-20 17:57:11 +0800

    Silicon/Hisilicon: Separate PlatformArch.h
    As the mocro of PlatformArch.h is platform special, so Separate
    PlatformArch.h to Hi1610,Hi1616,Hi1620 for unifying D0x.
    
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Zhou You <zhouyou17@huawei.com>
    Signed-off-by: Ming Huang <ming.huang@linaro.org>

---
+++ Silicon/Hisilicon/Hi1620/Include/PlatformArch.h
@@ -0,0 +1,34 @@
+/** @file
+*
+*  Copyright (c) 2015, Hisilicon Limited. All rights reserved.
+*  Copyright (c) 2015, Linaro Limited. All rights reserved.
+*
+*  This program and the accompanying materials
+*  are licensed and made available under the terms and conditions of the BSD License
+*  which accompanies this distribution.  The full text of the license may be found at
+*  http://opensource.org/licenses/bsd-license.php
+*
+*  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+*
+**/
+
+
+
+#ifndef _PLATFORM_ARCH_H_
+#define _PLATFORM_ARCH_H_
+
+#define MAX_SOCKET      2
+#define MAX_DIE         4
+#define MAX_DDRC        4
+#define MAX_NODE        (MAX_SOCKET * MAX_DIE)
+#define MAX_CHANNEL     8
+#define MAX_DIMM        2
+#define MAX_RANK_CH     8
+#define MAX_RANK_DIMM   4
+// Max NUMA node number for each node type
+#define MAX_NUM_PER_TYPE 8
+
+
+#endif
+
